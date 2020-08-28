b570d654a8bb166dee89127d56dee9cbbf383ad6
sourav santra <souravsantra59580@gmail.com>
2020-08-29 00:28:53 +0530

    2nd commit(29/08/2020)

--- public/lang/js/welcome.js
+++ public/lang/js/welcome.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-    $(".welcome-form-input").each(function(){
-        $(this).click(function(){
+$(document).ready(function () {
+    $(".welcome-form-input").each(function () {
+        $(this).click(function () {
             // hide placeholder
-            $(this).attr("placeholder","");
+            $(this).attr("placeholder", "");
 
             // animate placeholder
             var parent = this.parentElement;
@@ -12,6 +12,138 @@ $(document).ready(function(){
 
 
 
-          })
+        })
+    })
+})
+
+//slide on next
+$(document).ready(function () {
+    $(".step-1-next-btn").click(function (e) {
+        e.preventDefault();
+        empty_filed_validation("step-1", "step-2");
+    })
+
+    $(".step-2-next-btn").click(function (e) {
+        e.preventDefault();
+        empty_filed_validation("step-2", "step-3")
+    })
+    $(".step-3-next-btn").click(function (e) {
+        e.preventDefault();
+        empty_filed_validation("step-3", "step-4")
+    })
+})
+
+//empty filed validation
+function empty_filed_validation(first_class, second_class) {
+    var container = document.getElementsByClassName(first_class)[0];
+    var input = container.getElementsByClassName("required");
+    var url = container.getElementsByClassName("url");
+    var temp = [];
+    $(input).each(function (i) {
+     if($(this).val().trim()==""){
+         if(this.nextSibling.nodeName =="SMALL")
+         {
+            this.nextSibling.remove();
+         }
+         $(this).addClass("border-danger");
+         $("<small class='text-danger required-notice'><i class='fa fa-warning'></i> This filed can't be empty.</small>").insertAfter(this);
+     }else{
+         temp[i] = $(this).val().trim();
+         if(this.type == "email")
+         {
+            emailValidate(this);
+         } 
+     }
+    })
+
+    // url validation 
+    $(url).each(function(){
+        if($(this).val().trim()!=""){
+            validate_url(this);
+        }
+    })
+
+    // slide if all required filed is not empty
+    if(temp.length==input.length && $(".required-notice").length==0){
+        $("."+first_class).addClass("d-none");
+        $("."+second_class).removeClass("d-none");
+        $("."+second_class).addClass("animate__animated animate__slideInRight");
+    }
+
+    //remove required message on input
+    $(input).each(function () {
+        $(this).on("input",function(){
+            if(this.nextSibling.nodeName=='SMALL')
+            {
+                $(this).removeClass("border-danger");
+                this.nextSibling.remove();
+            }
+        })
       })
-  })
+
+       //remove required message on url
+    $(url).each(function () {
+        $(this).on("input",function(){
+            if(this.nextSibling.nodeName=='SMALL')
+            {
+                $(this).removeClass("border-danger");
+                this.nextSibling.remove();
+            }
+        })
+      })
+}
+
+//email validate
+function emailValidate(input)
+{
+    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    if(!reg.test(input.value)){
+        if(input.nextSibling.nodeName =="SMALL")
+         {
+            input.nextSibling.remove();
+         }
+         $(input).addClass("border-danger");
+         $("<small class='text-danger required-notice'><i class='fa fa-warning'></i> Enter a valid email.</small>").insertAfter(input);
+    }
+}
+
+// URL validation
+ function validate_url(input)
+ {
+     var reg = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/; 
+     if(!reg.test(input.value)){
+        if(input.nextSibling.nodeName =="SMALL")
+        {
+           input.nextSibling.remove();
+        }
+        $(input).addClass("border-danger");
+        $("<small class='text-danger required-notice'><i class='fa fa-warning'></i> Enter a valid URL.</small>").insertAfter(input);
+     }
+ }
+
+
+//slide on back
+$(document).ready(function () {
+    $(".step-2-back-btn").click(function (e) {
+        e.preventDefault();
+        $(".step-2").addClass("d-none");
+        $(".step-1").removeClass("d-none");
+        $(".step-1").addClass("animate__animated animate__slideInLeft");
+    })
+    $(".step-3-back-btn").click(function (e) {
+        e.preventDefault();
+        $(".step-3").addClass("d-none");
+        $(".step-2").removeClass("d-none");
+        $(".step-2").removeClass("animate__animated animate__slideInRight");
+        $(".step-2").addClass("animate__animated animate__slideInLeft");
+    })
+
+    $(".step-4-back-btn").click(function (e) {
+        e.preventDefault();
+        $(".step-4").addClass("d-none");
+        $(".step-3").removeClass("d-none");
+        $(".step-3").removeClass("animate__animated animate__slideInRight");
+        $(".step-3").addClass("animate__animated animate__slideInLeft");
+    })
+
+})
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,7 +23,7 @@ Wap Erp Solutions
     <div class="col-md-6 p-0 welcome-image">
 
     </div>
-    <div class="col-md-6 py-5">
+    <div class="col-md-6 py-5 overflow-hidden">
 
       <div class="branding">
         <h1>WES</h1>
@@ -33,72 +33,72 @@ Wap Erp Solutions
       <div class="welcome-form p-4">
         <form class="signup-form" autocomplete="off">
           <!-- start step 1 -->
-          <div class="step-1 d-none">
+          <div class="step-1">
             <div class="form-group mb-4 overflow-hidden">
               <label class="d-none">Company name</label>
-              <input type="text" name="company-name" class="form-control welcome-form-input rounded-0" placeholder="COMPANY NAME" />
+              <input type="text" name="company-name" class="form-control welcome-form-input rounded-0 required" placeholder="COMPANY NAME" />
             </div>
 
             <div class="form-group mb-4 overflow-hidden">
               <label class="d-none">Tagline</label>
-              <input type="text" name="tagline" class="form-control welcome-form-input rounded-0" placeholder="TAGLINE"/>
+              <input type="text" name="tagline" class="form-control welcome-form-input rounded-0" placeholder="TAGLINE" />
             </div>
             <div class="form-group mb-4 overflow-hidden">
               <label class="d-none">Website</label>
-              <input type="text" name="website" class="form-control welcome-form-input rounded-0" placeholder="WEBSITE"/>
+              <input type="website" name="website" class="form-control welcome-form-input rounded-0 url" placeholder="WEBSITE" />
             </div>
 
             <div class="form-group mb-4 overflow-hidden">
               <label class="d-none">Email id</label>
-              <input type="text" name="email" class="form-control welcome-form-input rounded-0" placeholder="EMAIL ID"/>
+              <input type="email" name="email" class="form-control welcome-form-input rounded-0 required" placeholder="EMAIL ID" />
             </div>
 
             <div class="form-group mb-4 overflow-hidden">
               <label class="d-none">Founder</label>
-              <input type="text" name="founder" class="form-control welcome-form-input rounded-0" placeholder="FOUNDER"/>
+              <input type="text" name="founder" class="form-control welcome-form-input rounded-0 required" placeholder="FOUNDER" />
             </div>
 
             <div class="form-group mb-5 overflow-hidden">
               <label class="d-none">Founder Email Id</label>
-              <input type="text" name="founderemail" class="form-control welcome-form-input rounded-0" placeholder="FOUNDER EMAIL ID"/>
+              <input type="email" name="founderemail" class="form-control welcome-form-input rounded-0 required" placeholder="FOUNDER EMAIL ID" />
             </div>
-           
+
             <div class="form-group">
               <button type="submit" class="btn float-right next-btn step-1-next-btn">NEXT <i class="fa fa-angle-double-right"></i></button>
             </div>
-            
+
           </div>
           <!-- end step 1 -->
           <!-- start step 2 -->
-          <div class="step-2">
+          <div class="step-2 d-none">
             <div class="form-group mb-4 overflow-hidden">
               <label class="d-none">Contact number</label>
-              <input type="number" name="contactnumber" class="form-control welcome-form-input rounded-0" placeholder="CONTACT NUMBER"/>
+              <input type="number" name="contactnumber" class="form-control welcome-form-input rounded-0 required" placeholder="CONTACT NUMBER" />
             </div>
 
             <div class="form-group mb-4 overflow-hidden">
               <label class="d-none">Street Address</label>
-              <input type="text" name="streetaddress" class="form-control welcome-form-input rounded-0" placeholder="STREET ADDRESS"/>
+              <input type="text" name="streetaddress" class="form-control welcome-form-input rounded-0 required" placeholder="STREET ADDRESS" />
             </div>
 
             <div class="form-group mb-4 overflow-hidden">
               <label class="d-none">City</label>
-              <input type="text" name="city" class="form-control welcome-form-input rounded-0" placeholder="CITY"/>
+              <input type="text" name="city" class="form-control welcome-form-input rounded-0 required" placeholder="CITY" />
             </div>
 
             <div class="form-group mb-4 overflow-hidden">
               <label class="d-none">State</label>
-              <input type="text" name="state" class="form-control welcome-form-input rounded-0" placeholder="STATE"/>
+              <input type="text" name="state" class="form-control welcome-form-input rounded-0 required" placeholder="STATE" />
             </div>
 
             <div class="form-group mb-4 overflow-hidden">
               <label class="d-none">Country</label>
-              <input type="text" name="country" class="form-control welcome-form-input rounded-0" placeholder="COUNTRY"/>
+              <input type="text" name="country" class="form-control welcome-form-input rounded-0 required" placeholder="COUNTRY" />
             </div>
 
             <div class="form-group mb-5 overflow-hidden">
               <label class="d-none">Pincode</label>
-              <input type="number" name="pincode" class="form-control welcome-form-input rounded-0" placeholder="PINCODE"/>
+              <input type="number" name="pincode" class="form-control welcome-form-input rounded-0 required" placeholder="PINCODE" />
             </div>
 
             <div class="form-group">
@@ -108,10 +108,67 @@ Wap Erp Solutions
             <div class="form-group">
               <button type="submit" class="btn float-right next-btn step-2-next-btn">NEXT <i class="fa fa-angle-double-right"></i></button>
             </div>
+          </div>
+          <!-- end step 2-->
+          <!-- start step 3-->
+          <div class="step-3 d-none">
+            <div class="form-group mb-5 overflow-hidden">
+              <label class="d-none">GSTIN</label>
+              <input type="text" name="gstin" class="form-control welcome-form-input rounded-0" placeholder="GSTIN" />
+            </div>
+            <div class="form-group mb-5 overflow-hidden">
+              <label class="d-none">Ofice start at</label>
+              <input type="time" name="start-at" class="form-control welcome-form-input rounded-0 required" />
+            </div>
+            <div class="form-group mb-5 overflow-hidden">
+              <label class="d-none">Ofice ends at</label>
+              <input type="time" name="end-at" class="form-control welcome-form-input rounded-0 required" />
+            </div>
+            <div class="form-group mb-5 overflow-hidden">
+              <label class="d-none">Established in</label>
+              <input type="date" name="est-at" class="form-control welcome-form-input rounded-0 required" />
+            </div>
+            <div class="form-group mb-5 overflow-hidden">
+              <label class="d-none">Facebook page url</label>
+              <input type="url" name="facebook" class="form-control welcome-form-input rounded-0 url" placeholder="FACEBOOK PAGE URL" />
+            </div>
+            <div class="form-group mb-5 overflow-hidden">
+              <label class="d-none">Twitter page url</label>
+              <input type="url" name="twitter" class="form-control welcome-form-input rounded-0 url" placeholder="TWITTER PAGE URL" />
+            </div>
+            <div class="form-group">
+              <button type="submit" class="btn float-left back-btn step-3-back-btn"><i class="fa fa-angle-double-left"> </i> BACK </button>
+            </div>
+
+            <div class="form-group">
+              <button type="submit" class="btn float-right next-btn step-3-next-btn">NEXT <i class="fa fa-angle-double-right"></i></button>
+            </div>
+          </div>
+          <!-- end step 3 -->
+          <!-- start step 4-->
+          <div class="step-4 d-none">
+            <div class="form-group mb-5 overflow-hidden">
+              <label class="d-none">What's app number</label>
+              <input type="number" name="whats-app" class="form-control welcome-form-input rounded-0" placeholder="WHATS APP NUMBER" />
+            </div>
+            <div class="form-group mb-5 overflow-hidden">
+              <label>Category</label>
+              <select class="form-control required" name="category">
+                <option>Education</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <button type="submit" class="btn float-left back-btn step-4-back-btn"><i class="fa fa-angle-double-left"> </i> BACK </button>
+            </div>
+
+            <div class="form-group">
+              <button type="submit" class="btn float-right submit-btn">SUBMIT</button>
+            </div>
 
 
           </div>
-          <!-- end step 2-->
+
+          <!-- end step 4-->
         </form>
       </div>
 
